18a8f60a37f5ce75e6b089cfbe0afba96364be5e
Liam <lmmurray@student.fullsail.edu>
2019-09-07 21:38:45 -0400

    added iterator proxy

--- DarkSeer/source/Components/private/InputBuffer.h
+++ DarkSeer/source/Components/private/InputBuffer.h
@@ -9,9 +9,24 @@ struct KeyCodeSet
                             _mm256_or_si256(m_keyCodeSetInternal, MM256FlagsLUT[to_underlying_type(keyCode)]);
         }
 
-    private:
+    protected:
         __m256i m_keyCodeSetInternal;
         friend struct InputFrame;
+};
+
+// set of keys that must be held down but is considered OnFirstKeyPress only when the activator key was released on the previous
+// input frame
+struct ActivatedKeyCodeSet : public KeyCodeSet
+{
+        inline ActivatedKeyCodeSet(std::initializer_list<KeyCode> keyCodes, KeyCode activatorKey) : KeyCodeSet(keyCodes)
+        {
+                m_keyCodeSetInternal = _mm256_or_si256(m_keyCodeSetInternal, MM256FlagsLUT[to_underlying_type(activatorKey)]);
+                m_activatorKeyCodeInternal = activatorKey;
+        }
+
+    protected:
+        KeyCode m_activatorKeyCodeInternal;
+        friend struct InputBuffer;
 };
 
 struct alignas(CACHE_LINE) InputFrame
@@ -78,16 +93,19 @@ struct alignas(CACHE_LINE) InputFrame
         }
 };
 
+//================================================================
+// Multithreaded Circular Buffer (Single Producer - Single Consumer)
 struct InputBuffer
 {
     public:
+        struct iterator_proxy;
         struct iterator
         {
             private:
-                // using value_type      = InputFrame;
+                using value_type      = iterator_proxy;
                 using difference_type = int64_t;
-                using pointer         = InputFrame*;
-                using reference       = InputFrame&;
+                using pointer         = iterator_proxy*;
+                using reference       = iterator_proxy&;
 
                 const InputBuffer& m_owner;
                 int64_t            m_currIndex;
@@ -96,10 +114,6 @@ struct InputBuffer
                 {}
                 inline iterator(const iterator& other) : m_owner(other.m_owner), m_currIndex(other.m_currIndex)
                 {}
-                inline const InputFrame& previous() const
-                {
-                        return m_owner.m_inputFrames[m_currIndex - 1];
-                }
 
             public:
                 // prefix
@@ -126,6 +140,7 @@ struct InputBuffer
                         m_currIndex--;
                         return temp;
                 }
+                //
                 inline iterator& operator+=(int64_t offset)
                 {
                         m_currIndex += offset;
@@ -144,13 +159,13 @@ struct InputBuffer
                 {
                         return iterator(m_owner, m_currIndex + rhs);
                 }
-                inline const InputFrame& operator*() const
+                inline const iterator_proxy& operator*() const
                 {
-                        return m_owner.m_inputFrames[InputBuffer::MASK & m_currIndex];
+                        return reinterpret_cast<const iterator_proxy&>(*this);
                 }
-                inline const InputFrame* operator->() const
+                inline const iterator_proxy* operator->() const
                 {
-                        return &m_owner.m_inputFrames[InputBuffer::MASK & m_currIndex];
+                        return reinterpret_cast<const iterator_proxy*>(this);
                 }
                 inline bool operator==(const iterator& other) const
                 {
@@ -176,40 +191,64 @@ struct InputBuffer
                 {
                         return m_currIndex > other.m_currIndex;
                 }
+                friend struct InputBuffer;
+        };
+        struct iterator_proxy : iterator
+        {
+            private:
+                inline const InputFrame& previous_input_frame() const
+                {
+                        return m_owner.m_inputFrames[m_currIndex - 1];
+                }
+                inline const InputFrame& this_input_frame() const
+                {
+                        return m_owner.m_inputFrames[InputBuffer::MASK & m_currIndex];
+                }
 
+            public:
+                inline const InputFrame& operator*() const
+                {
+                        return this_input_frame();
+                }
+                inline const InputFrame* operator->() const
+                {
+                        return &this_input_frame();
+                }
                 inline bool IsKeyBeginPressFrame(KeyCode keyCode) const
                 {
-                        return (**this).IsKeyPressFrame(keyCode) && !previous().IsKeyHeld(keyCode);
+                        return this_input_frame().IsKeyPressFrame(keyCode) && !previous_input_frame().IsKeyHeld(keyCode);
                 }
-				inline bool IsKeySetBeginPress(KeyCodeSet keyCodeSet) const
-				{
-                        return (**this).IsKeySetHeld(keyCodeSet) && !previous().IsKeySetHeld(keyCodeSet);
-				}
                 inline bool IsKeyPressFrame(KeyCode keyCode) const
                 {
-                        return (**this).IsKeyPressFrame(keyCode);
+                        return this_input_frame().IsKeyPressFrame(keyCode);
                 }
                 inline bool IsKeyReleaseFrame(KeyCode keyCode) const
                 {
-                        return (**this).IsKeyReleaseFrame(keyCode);
+                        return this_input_frame().IsKeyReleaseFrame(keyCode);
                 }
                 inline bool IsKeyHeld(KeyCode keyCode) const
                 {
-                        return (**this).IsKeyHeld(keyCode);
-                }
-                inline bool IsKeySetHeld(KeyCodeSet keyCodeSet) const
-                {
-                        return (**this).IsKeySetHeld(keyCodeSet);
+                        return this_input_frame().IsKeyHeld(keyCode);
                 }
                 inline bool IsKeyReleased(KeyCode keyCode) const
                 {
-                        return !(**this).IsKeyHeld(keyCode);
+                        return !this_input_frame().IsKeyHeld(keyCode);
                 }
-                friend struct InputBuffer;
+                inline bool IsKeySetBeginPress(KeyCodeSet keyCodeSet) const
+                {
+                        return this_input_frame().IsKeySetHeld(keyCodeSet) && !previous_input_frame().IsKeySetHeld(keyCodeSet);
+                }
+                inline bool IsKeySetBeginPress(ActivatedKeyCodeSet keyCodeSet) const
+                {
+                        return this_input_frame().IsKeySetHeld(keyCodeSet) &&
+                               !previous_input_frame().IsKeyHeld(keyCodeSet.m_activatorKeyCodeInternal);
+                }
+                inline bool IsKeySetHeld(KeyCodeSet keyCodeSet) const
+                {
+                        return this_input_frame().IsKeySetHeld(keyCodeSet);
+                }
         };
-
         //================================================================
-        // Multithreaded Circular Buffer (Single Producer - Single Consumer)
         static constexpr uint64_t MAX_INPUT_FRAMES_PER_FRAME = saturatePowerOf2(10000U);
         static constexpr uint64_t MASK                       = MAX_INPUT_FRAMES_PER_FRAME - 1;
         InputFrame*               m_inputFrames;
@@ -219,8 +258,6 @@ struct InputBuffer
         // Signal variables (per-input-update freeze frame captures of m_bottom and m_top)
         const int64_t m_currFrameBottom;
         const int64_t m_currFrameTop;
-        //================================================================
-
         //================================================================
         // producer thread mutators/accessors
         void                     push_back(InputFrame inputFrame);
@@ -254,5 +291,3 @@ struct InputBuffer
         ~InputBuffer();
         friend struct SingletonInput;
 };
-
-KeyCodeSet CreateKeyCodeSet(std::initializer_list<KeyCode> keyCodes);
--- DarkSeer/source/Systems/private/InputSystem.cpp
+++ DarkSeer/source/Systems/private/InputSystem.cpp
@@ -3,6 +3,9 @@
 
 #include <SingletonConsole.h>
 #include <SingletonInput.h>
+
+const ActivatedKeyCodeSet _private_set = ActivatedKeyCodeSet({KeyCode::A, KeyCode::ShiftLeft, KeyCode::S}, KeyCode::A);
+
 void InputSystem::PreUpdate(EntityAdmin* entityAdmin)
 {
         auto singlInput       = entityAdmin->GetSingletonInput();
@@ -10,13 +13,9 @@ void InputSystem::PreUpdate(EntityAdmin* entityAdmin)
         auto singlInputBuffer = singlInput->GetInputBuffer();
         singlInputBuffer->Signal();
 
-        for (auto itr = singlInputBuffer->begin(); itr != singlInputBuffer->end(); itr++)
+        for (auto& itr : *singlInputBuffer)
         {
-                if (itr.IsKeyPressFrame(KeyCode::ControlRight))
-                        std::cout << "C\n";
-                if (itr.IsKeySetBeginPress(KeyCodeSet({KeyCode::A, KeyCode::ShiftLeft, KeyCode::S})))
+                if (itr.IsKeySetBeginPress(_private_set))
                         std::cout << "HelloSIMD\n";
-                if (itr.IsKeyHeld(KeyCode::A))
-                        std::cout << "A[][]";
         }
 }
